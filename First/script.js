927fd317b10be7aefde462e7d3d2a12c2cf34077
Abhay Chouhan <abhaychouhan0503@gmail.com>
2026-05-12 20:30:39 +0530

    Initial commit

---
+++ First/script.js
@@ -0,0 +1,16 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > 50){
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});             
